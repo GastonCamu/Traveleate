bbb8b4a0d4ab8ddf336d9d458f4ddc5665fb5e25
Gastón Camú <camugaston2002@gmail.com>
2024-05-14 23:14:45 -0300

    Se agrego bootstrap al proyecto
    Ademas se agrego el apartado de acerca de nosotros.

--- Traveleate-Front-Angular17/src/app/app.routes.ts
+++ Traveleate-Front-Angular17/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ViajesComponent } from './pages/viajes/viajes.component';
 import { ReservarComponent } from './pages/reservar/reservar.component';
 import { PreReservaComponent } from './pages/pre-reserva/pre-reserva.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -10,4 +11,5 @@ export const routes: Routes = [
     {path: 'viajes/:localidadOrigen/:localidadDestino/:fecha', component: ViajesComponent},
     {path: 'pre-reserva', component: PreReservaComponent},
     {path: 'reservar', component: ReservarComponent},
+    {path: 'nosotros', component: NosotrosComponent},
 ];
